1e3d5943b45eac429d35d6f124c3f04c4379f6c4
Stacydina <stacydina6@gmail.com>
2020-11-25 08:12:15 +0300

    styling added

--- food.js
+++ food.js
@@ -16,25 +16,58 @@ const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${
   
  recipe()
  .then(data=> {
- // console.log( 'resolved', data.hits)
+ console.log( 'resolved', data.hits)
   let table = document.createElement("table");
       let thead = document.createElement("thead");
       let trow = document.createElement("tr");
       thead.appendChild(trow);
       table.appendChild(thead);
+      
+  var header = table.createTHead();
+  var row = header.insertRow(0);
+  var cell = row.insertCell(0);
+  cell.innerHTML = "<b>Meal name</b>";
+  var cell = row.insertCell(1);
+  cell.innerHTML = "<b>Diet type</b>";
+
+  var cell = row.insertCell(2);
+  cell.innerHTML = "<b>Health Labels</b>";
+
+  var cell = row.insertCell(3);
+  cell.innerHTML = "<b>Waiting time</b>";
+
+  var cell = row.insertCell(4);
+  cell.innerHTML = "<b>Will contain</b>";
+
+
       // let overallTable = "";
       // overallTable += `${data.hits[0].recipe.label}
       //   ${data.hits[0].recipe.calories}`;
       for (let i = 0; i < (`${data.hits.recipe}`).length; i++) {
         //console.log(i);
         let trow = document.createElement("tr");
-        
+       
         let td = document.createElement("td");
         td.appendChild(document.createTextNode(`${data.hits[i].recipe.label}`));
         trow.appendChild(td);
+
         td = document.createElement("td");
-        td.appendChild(document.createTextNode(`${data.hits[i].recipe.calories}`));
+        td.appendChild(document.createTextNode(`${data.hits[i].recipe.dietLabels}`));
         trow.appendChild(td);
+
+        td = document.createElement("td");
+        td.appendChild(document.createTextNode(`${data.hits[i].recipe.healthLabels[2]}`));
+        trow.appendChild(td);
+
+        td = document.createElement("td");
+        td.appendChild(document.createTextNode(`${data.hits[i].recipe.totalTime}`));
+        trow.appendChild(td);
+
+        td = document.createElement("td");
+        td.appendChild(document.createTextNode(`${data.hits[i].recipe.ingredientLines[1]}`));
+        trow.appendChild(td);
+
+       
         table.appendChild(trow);
       }
       //console.log(overallTable);
